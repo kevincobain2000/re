6c38952845666c9dcb76d7aaf775862744ebdefe
Pulkit Kathuria <kevincobain2000@gmail.com>
2023-05-23 19:49:56 +0900

    vet

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ const README_URL = "https://github.com/kevincobain2000/re"
 
 func BenchmarkCodelinesLocal(t *testing.B) {
 	for i := 0; i < t.N; i++ {
-		commands := pkg.NewReadmeHandler(RELATIVE_README_PATH).Codelines()
+		commands := pkg.NewReadmeHandler(RELATIVE_README_PATH, "").Codelines()
 		if len(commands) == 0 {
 			t.Error("No commands found")
 		}
@@ -19,7 +19,7 @@ func BenchmarkCodelinesLocal(t *testing.B) {
 }
 func BenchmarkCodelinesRemote(t *testing.B) {
 	for i := 0; i < t.N; i++ {
-		commands := pkg.NewReadmeHandler(README_URL).Codelines()
+		commands := pkg.NewReadmeHandler(README_URL, "").Codelines()
 		if len(commands) == 0 {
 			t.Error("No commands found")
 		}
--- pkg/readme_test.go
+++ pkg/readme_test.go
@@ -9,16 +9,16 @@ import (
 const RELATIVE_README_PATH = "../README.md"
 
 func TestRead(t *testing.T) {
-	got := NewReadmeHandler(RELATIVE_README_PATH).readLocal()
+	got := NewReadmeHandler(RELATIVE_README_PATH, "").readLocal()
 	assert.Assert(t, len(got) > 0)
 }
 
 func TestParseCodeBlocks(t *testing.T) {
-	got := NewReadmeHandler(RELATIVE_README_PATH).parseCodeBlocks()
+	got := NewReadmeHandler(RELATIVE_README_PATH, "").parseCodeBlocks()
 	assert.Assert(t, got != nil)
 	assert.Assert(t, len(got) > 0)
 }
 func TestParseCodelines(t *testing.T) {
-	got := NewReadmeHandler(RELATIVE_README_PATH).Codelines()
+	got := NewReadmeHandler(RELATIVE_README_PATH, "").Codelines()
 	assert.Assert(t, len(got) > 0)
 }
